8c7eefbe06f134c0ea2175a0b4a9d0ea5492439c
emon5122 <emon5122@gmail.com>
2023-06-24 01:29:12 +0600

    Few changes

---
+++ src/components/header.tsx
@@ -0,0 +1,15 @@
+const Headers = () => {
+    return (
+        <div className="flex flex-row gap-2">
+            <a href="/">
+                <img
+                    src="vector/default.svg"
+                    alt="logo"
+                    className="h-32 aspect-auto"
+                />
+            </a>
+            <p>About us</p>
+        </div>
+    );
+};
+export default Headers;
